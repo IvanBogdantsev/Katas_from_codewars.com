b22d2194ecfa385cd2c194deca68494e5ac263d3
IvanBogdantsev <bogdantsev.st@gmail.com>
2023-10-14 01:05:32 +0100

    ಠ_ಠ

--- Katas from codewars.com.playground/Contents.swift
+++ Katas from codewars.com.playground/Contents.swift
@@ -58,6 +58,7 @@ func zeros(_ n: UInt32) -> UInt32 {
  Every word and punctuation mark should be seperated by a space ' '.
  There should be no trailing whitespace
  */
+
  func toNato(_ words: String) -> String {
  return words.map { natoAlphabet[$0.uppercased()] == nil ? String($0) : natoAlphabet[$0.uppercased()]! }.filter { $0 != " " }.joined(separator: " ")
  }
@@ -423,3 +424,15 @@ func rgb(_ red: Int, _ green: Int, _ blue: Int) -> String {
 var dict: [Int : Int] = [1 : 1]
 dict[1] = 0
 dict[1]
+
+/*
+ In the morning all the doors in the school are closed. The school is quite big: there are N doors. Then pupils start coming. It might be hard to believe, but all of them want to study! Also, there are exactly N children studying in this school, and they come one by one.
+
+ When these strange children pass by some doors they change their status (i.e. Open -> Closed, Closed -> Open). Each student has their number, and each i-th student alters the status of every i-th door. For example: when the first child comes to the schools, he changes every first door (he opens all of them). The second one changes the status of every second door (he closes some doors: the 2nd, the 4th and so on). Finally, when the last one – the n-th – comes to the school, he changes the status of each n-th door (there's only one such door, though).
+
+ You need to count how many doors are left opened after all the students have come.
+ */
+
+func doors(_ n: Int) -> Int {
+    return Array(1...n).map { sqrt(Double($0)).truncatingRemainder(dividingBy: 1) == 0}.filter { $0 }.count
+}
